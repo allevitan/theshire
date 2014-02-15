53011ec347d98743e46f17a8c59db5511fd925e7
Abe Levitan <allevitan@gmail.com>
2014-02-15 13:42:35 -0500

    iteration 1

---
+++ js/main.js
@@ -0,0 +1,65 @@
+window.onload = function(){
+    backgroundify();
+};
+
+window.onresize = function(){
+    backgroundify();
+};
+
+
+function backgroundify(){
+    var body = document.body;
+    var html = document.documentElement;
+    var docheight = Math.max( body.scrollHeight, body.offsetHeight, 
+			      html.clientHeight, html.scrollHeight,
+			      html.offsetHeight );
+    var match; var bmatch; var flow; var bflow;
+    var width = Math.max(window.innerWidth, window.outerWidth);
+    var height = Math.max(window.innerHeight, window.outerHeight) + 100;
+    var background = document.getElementById('background');
+    var bwidth = background.width
+    var bheight = background.height;
+    var scrollOnHeight = true;
+    var maxbuff = (width * bheight/bwidth - height);
+    var center = maxbuff / 2;
+    if (width/height >= bwidth/bheight){
+	background.style.width = width;
+	background.style.height = 'auto';
+    } else {
+	background.style.height = height;
+	background.style.width = 'auto';
+	scrollOnHeight = false;
+	maxbuff = (height * bwidth/bheight - width);
+	center = maxbuff / 2;
+    }
+    console.log(maxbuff);
+    var scrollbuff = docheight - height;
+    var scrollstart = 0;
+    var scrollconst = maxbuff / scrollbuff;
+    if (scrollbuff / 2 < maxbuff){
+	scrollstart = -(center - scrollbuff / 4);
+	scrollconst = 0.5;
+    }
+    var scrolled = document.body.scrollTop;
+    if (scrollOnHeight){
+	background.style.top = scrollstart - scrolled * scrollconst;
+	background.style.left = 0;
+    } else {
+	background.style.left = scrollstart - scrolled * scrollconst;
+	background.style.top = 0;
+    }
+    console.log(scrollstart,scrollconst);
+
+    window.onscroll = function(){
+	var scrolled = document.body.scrollTop;
+	if (scrollOnHeight){
+	    background.style.top = scrollstart - scrolled * scrollconst;
+	} else {
+	    background.style.left = scrollstart - scrolled * scrollconst;
+	}
+	console.log(scrollstart + scrolled * scrollconst);
+    }
+
+};
+
+
